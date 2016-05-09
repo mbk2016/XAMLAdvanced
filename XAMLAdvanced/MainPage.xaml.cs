ed3a1b09d9e70ef0ae20fb4866931e04557a7c83
mbk2016 <mateuszblaszczyk94@gmail.com>
2016-05-09 20:48:02 +0200

    Added app

---
+++ XAMLAdvanced/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace XAMLAdvanced
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            var storyboard = new Storyboard();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void ClickedChangeOpacity(object sender, RoutedEventArgs e)
+        {
+            StoryboardChangeOpacity.Begin();      
+        }
+
+        private void ClickedMoveLeft(object sender, RoutedEventArgs e)
+        {
+            StoryboardMoveToLeft.Begin();
+        }
+
+        private void ClickedMoveRight(object sender, RoutedEventArgs e)
+        {
+            StoryboardMoveToRight.Begin();
+
+            int zmienna = 3;
+            string wyraz = "czesc";
+        }
+
+        private void ClickedMoveToCenter(object sender, RoutedEventArgs e)
+        {
+            StoryboardMoveToCenter.Begin();
+        }
+
+        private void ClickedRotate(object sender, RoutedEventArgs e)
+        {
+            //StoryboardRotate.Begin();
+            //ButtonRotate.Visibility = Visibility.Collapsed;
+            //Task.Delay(200);
+            //ButtonStopRotating.Visibility = Visibility.Visible;
+            if(TBButtonRotate.Text == "Obracaj")
+            {
+                StoryboardRotate.Begin();
+                TBButtonRotate.Text = "Stop";
+            }
+            else if(TBButtonRotate.Text == "Stop")
+            {
+                StoryboardStopRotating.Begin();
+                TBButtonRotate.Text = "Obracaj";
+            }
+        }
+
+    }
+
+}
---
+++ XAMLAdvanced/Przycisk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAMLAdvanced
+{
+    public class Przycisk
+    {
+        public string Nazwa { get; set; }
+        public string CzasTrwania { get; set; }
+        public bool IsAutoreverse { get; set; }
+    }
+}
